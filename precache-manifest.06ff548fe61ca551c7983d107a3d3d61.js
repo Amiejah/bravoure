7f8b0234bdf95bf1bb20e577c6f181e13c6ac67e
Amir Swaleh <info@humpff.com>
2019-07-09 07:13:42 +0200

    Updates

---
+++ precache-manifest.06ff548fe61ca551c7983d107a3d3d61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64af9500e1e7a86c5641b9ac3a24603f",
+    "url": "/bravoure/index.html"
+  },
+  {
+    "revision": "8daeee518280affcf784",
+    "url": "/bravoure/static/css/2.bd160257.chunk.css"
+  },
+  {
+    "revision": "715ed73ca632bf1a598e",
+    "url": "/bravoure/static/css/main.d8e271a6.chunk.css"
+  },
+  {
+    "revision": "8daeee518280affcf784",
+    "url": "/bravoure/static/js/2.cd621abb.chunk.js"
+  },
+  {
+    "revision": "715ed73ca632bf1a598e",
+    "url": "/bravoure/static/js/main.9f41a4d7.chunk.js"
+  },
+  {
+    "revision": "926cbe314871960955dc",
+    "url": "/bravoure/static/js/runtime~main.196dcd41.js"
+  }
+]);
